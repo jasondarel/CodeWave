195cfc6d4981b0fadc2748fc50a81b0cf4df4e2a
kevinn.s <vinn1864.s@gmail.com>
2024-12-03 21:12:00 +0700

    ...

--- CodeWave/resources/views/layout.blade.php
+++ CodeWave/resources/views/layout.blade.php
@@ -899,8 +899,8 @@
     @endif
 </head>
 
-<body class="flex">
-    <div class="sticky top-0 left-0 w-full p-5 flex flex-col gap-12 bg-white z-10">
+<body class="flex ">
+    <div class="sticky top-0 left-0 w-80 p-5 flex flex-col gap-12 bg-white z-10">
 
         <div>LOGO CODEWAVE DISINI</div>
         <div class="flex flex-col  gap-6">
