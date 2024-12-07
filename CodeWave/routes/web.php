8f494243b25c63a7a642602703d8e67b1e71c1bd
baswara123 <baswaradandi@gmail.com>
2024-12-07 15:01:42 +0700

    add inbox/id

--- CodeWave/routes/web.php
+++ CodeWave/routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DashboardController::class, 'dashboard'])
@@ -61,8 +62,32 @@ Route::get('/announcements/announcement1', function () {
     return view('announcement.announcement1');
 });
 
-Route::get('/inbox/1', function () {
-    return view('inbox.inboxmessage');
+Route::get('/inbox/{id}', function ($id) {
+    $notifications = [
+        1 => [
+            'title' => 'Successfully Enrolled',
+            'message' => 'You have successfully enrolled in the "Intro to JavaScript" course.',
+            'date' => 'December 4, 2024, 10:30 AM',
+        ],
+        2 => [
+            'title' => 'Course Completed',
+            'message' => 'You have completed the "Advanced Python Programming" course.',
+            'date' => 'December 3, 2024, 2:15 PM',
+        ],
+        3 => [
+            'title' => 'New Lesson Available',
+            'message' => 'A new lesson on "React State Management" is available in your course.',
+            'date' => 'December 2, 2024, 8:00 PM',
+        ],
+    ];
+
+    if (!array_key_exists($id, $notifications)) {
+        abort(404); // Show 404 error if ID doesn't exist
+    }
+
+    return response()->view('inbox.inboxmessage', [
+        'notification' => $notifications[$id]
+    ]);
 });
 
 Route::get('/my-courses/python-for-beginners', function () {
